b70a3add1fc6369b09ee8d0edfb22855013b52e3
Rohit Dhas <rohitdhas666@gmail.com>
2023-06-25 15:36:45 +0530

    create function: randomizeCase

---
+++ src/utils/cst-formatter/case.ts
@@ -0,0 +1,42 @@
+interface Imap {
+    [key: string]: string; 
+}
+
+const requiredTokenTypes = ["VariableDeclarator", "FunctionDeclaration", "ClassDeclaration"];
+const map: Imap = {};
+
+function randomizeCase(ast: any) {
+  ast.selectTokensByType('Identifier').forEach((token: any) => {
+    try {
+      if (requiredTokenTypes.includes(token.parentElement.parentElement.type)) {
+        if (!map[token.value]) {
+          const update = changeCaseRandomly(token.value);
+          map[token.value] = update;
+        }
+        const val = map[token.value];
+        token.value = val;
+        token._sourceCode = val;
+        token._sourceCodeLines = [val];
+      } else {
+        const val = map[token.value] || token.value;
+        token.value = val;
+        token._sourceCode = val;
+        token._sourceCodeLines = [val];
+      }
+    } catch(err) {
+      // handle token errors
+      // console.log(err);
+    }
+  });
+}
+
+function changeCaseRandomly(str: string) {
+  let modifiedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    const randomCase = Math.random() < 0.5 ? 'toUpperCase' : 'toLowerCase';
+    modifiedStr += str[i][randomCase]();
+  }
+  return modifiedStr;
+}
+
+module.exports = { randomizeCase };
