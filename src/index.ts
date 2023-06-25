a5ce4d17f9f01b59fdb37b589da5cbfbb605eace
Rohit Dhas <rohitdhas666@gmail.com>
2023-06-25 21:01:56 +0530

    implemented file validation

--- src/index.ts
+++ src/index.ts
@@ -36,24 +36,31 @@ const inputFilePath = argv._[0];
 const outputFilePath = argv._[1] || inputFilePath;
 
 // Validate file extension
-const supportedExtensions = ['.js', '.ts'];
+const supportedExtensions = ['.js'];
 const inputFileExtension = extname(inputFilePath.toString()).toLowerCase();
 
 if (!supportedExtensions.includes(inputFileExtension)) {
-  console.error(`❌ Unsupported file type. Only JavaScript and TypeScript files are supported currently.`);
+  console.error(`❌ Unsupported file type. Only JavaScript files are supported currently.`);
   process.exit(1);
 }
 
 try {
   const code = readFileSync(inputFilePath, 'utf-8');
   const shittifiedCode = shittify(code);
-
   writeFileSync(outputFilePath, shittifiedCode, {
     encoding: 'utf-8',
-    flag: argv.force ? 'w' : 'wx',
+    flag: argv.force || (inputFilePath === outputFilePath)  ? 'w' : 'wx',
   });
-} catch(err: any) {
-  if (err.code === 'ENOENT') {
-    console.error("❌ Invalid file path!");
+
+  console.log("Your code has been successfully shittified. 🎉");
+} catch(err) {
+  if (err instanceof Error) {
+    if (err.message.includes("EEXIST")) {
+      console.log(`❌ ERROR: ${outputFilePath} already exists. Use --force to override`)
+    } else if (err.message.includes("ENOENT")) {
+      console.error("❌ ERROR: Invalid file path!");
+    } else if (err.name === 'SyntaxError') {
+      console.log(`❌ ERROR: Can't parse file. Looks like ${inputFilePath} has syntax errors!`)
+    }
   }
 }
