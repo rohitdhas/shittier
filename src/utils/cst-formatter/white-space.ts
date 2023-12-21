beae38d88a9c30887b6734e11014284d901bc76b
Rohit Dhas <rohitdhas666@gmail.com>
2023-12-21 23:07:19 +0530

    fix: fixes infinite indentation bug

--- src/utils/cst-formatter/white-space.ts
+++ src/utils/cst-formatter/white-space.ts
@@ -1,22 +1,35 @@
 import { Token } from 'cst';
 
 function addInconsistentIndentation(code: string) {
-  const lines = code.split("\n");
+  const lines = code.split('\n');
   for (let i = 0; i < lines.length; i++) {
-    if (lines[i].trim() !== "") {
+    const text = lines[i].trim();
+
+    if (text !== '') {
       const indentation = getRandomWhitespace();
-      lines[i] = indentation + lines[i];
+      lines[i] = indentation + text;
     }
   }
-  return lines.join("\n");
+  return lines.join('\n');
 }
 
 function addRandomWhiteSpace(code: any) {
-  code.selectTokensByType("Punctuator").forEach((token: any) => {
+  code.selectTokensByType('Punctuator').forEach((token: any) => {
     try {
       const randomWhitespaceBefore = getRandomWhitespace();
-      const whitespaceBefore = new Token("Whitespace", randomWhitespaceBefore);
-      token.parentElement.insertChildBefore(whitespaceBefore, token);
+
+      const prevToken = token.getPreviousToken();
+      if (prevToken.isWhitespace) {
+        prevToken.value = randomWhitespaceBefore;
+        prevToken._sourceCode = randomWhitespaceBefore;
+        prevToken._sourceCodeLines = [randomWhitespaceBefore];
+      } else {
+        const whitespaceBefore = new Token(
+          'Whitespace',
+          randomWhitespaceBefore
+        );
+        token.parentElement.insertChildBefore(whitespaceBefore, token);
+      }
     } catch (err) {
       // TODO: Handle token errors
       // console.log(err);
@@ -25,7 +38,7 @@ function addRandomWhiteSpace(code: any) {
 }
 
 function getRandomWhitespace() {
-  const whitespaceOptions = [" ", "  ", "\t", "\t\t", "\t "];
+  const whitespaceOptions = [' ', '  ', '\t', '\t\t', '\t '];
   const randomIndex = Math.floor(Math.random() * whitespaceOptions.length);
   return whitespaceOptions[randomIndex];
 }
