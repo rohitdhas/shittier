64da02ddeefad689774126219baf10bf9909175b
toiletbril <s9und@outlook.com>
2024-01-25 20:17:04 +0300

    feat: Add comment blocks with garbage along whitespace

--- src/utils/cst-formatter/white-space.ts
+++ src/utils/cst-formatter/white-space.ts
@@ -17,18 +17,24 @@ function addRandomWhiteSpace(code: any) {
   code.selectTokensByType('Punctuator').forEach((token: any) => {
     try {
       const randomWhitespaceBefore = getRandomWhitespace();
-
+      const randomCommentBlockBefore = getRandomCommentBlocks();
       const prevToken = token.getPreviousToken();
       if (prevToken.isWhitespace) {
         prevToken.value = randomWhitespaceBefore;
         prevToken._sourceCode = randomWhitespaceBefore;
         prevToken._sourceCodeLines = [randomWhitespaceBefore];
       } else {
-        const whitespaceBefore = new Token(
-          'Whitespace',
-          randomWhitespaceBefore
-        );
-        token.parentElement.insertChildBefore(whitespaceBefore, token);
+        const shouldUseComments = (Math.random() < 0.2);
+        let distractionBefore = (shouldUseComments)
+          ? new Token(
+            'CommentBlock',
+            randomCommentBlockBefore
+          )
+          : new Token(
+            'Whitespace',
+            randomWhitespaceBefore
+          );
+        token.parentElement.insertChildBefore(distractionBefore, token);
       }
     } catch (err) {
       // TODO: Handle token errors
@@ -41,6 +47,14 @@ function getRandomWhitespace() {
   const whitespaceOptions = [' ', '  ', '\t', '\t\t', '\t '];
   const randomIndex = Math.floor(Math.random() * whitespaceOptions.length);
   return whitespaceOptions[randomIndex];
+}
+
+function getRandomCommentBlocks() {
+  const commentOptions = [
+    ':3', '.-.', 'hello world', 'T__T', 'i love javascript', 'why', ': - )'
+  ];
+  const randomIndex = Math.floor(Math.random() * commentOptions.length);
+  return ' ' + commentOptions[randomIndex] + ' ';
 }
 
 export { addInconsistentIndentation, addRandomWhiteSpace };
